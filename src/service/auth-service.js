432277f87fd0333ad1eb753c49af394ba482dd55
AlexWasHeree <victoralexandre2303@gmail.com>
2024-05-21 03:59:52 -0300

    first routes created

---
+++ src/service/auth-service.js
@@ -0,0 +1,12 @@
+import {compare, hash} from 'brypt';
+import prisma from '../prisma/client.js';
+import pkg from 'jsonwebtoken';
+import {sign} from pkg
+
+export const register = async (req, res) => {
+
+}
+
+export const login = async (req, res) => {
+  
+}
